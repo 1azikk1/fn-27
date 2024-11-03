766772d837bccaf2c0228ebd4aaf1b73c0f4a32a
1azikk1 <ahmadjonovazizbek856@gmail.com>
2024-11-03 18:03:57 +0500

    id generator qismini qo'shdim

--- 12 dars uyga vazifa/main.py
+++ 12 dars uyga vazifa/main.py
@@ -2,3 +2,17 @@ print('hello world')
 
 
 print("Salom dunyo")
+
+import random
+import string
+
+
+def id_generator(length=4):
+    random_id = string.digits
+    user_id = ""
+    for i in range(length):
+        user_id += random.choice(random_id)
+    return user_id
+
+
+print(id_generator())
